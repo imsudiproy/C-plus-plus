1c9fbe6e9ae47dbad6b9da629a75f7b158f1d666
Sudip Roy <59234028+imsudiproy@users.noreply.github.com>
2020-01-07 21:16:44 +0530

    Update Maximum of 4 numbers.cpp

--- Maximum of 4 numbers.cpp
+++ Maximum of 4 numbers.cpp
@@ -31,9 +31,9 @@ int max_of_four(int a, int b, int c, int d)
 }
 int main() {
     int a, b, c, d, max;
+    cout<<"Enter four numbers: ";
     scanf("%d %d %d %d", &a, &b, &c, &d);
     max_of_four(a, b,c, d);
-    //cout<<max_of_four(int a, int b, int c, int d);
     return 0;
 }
 
