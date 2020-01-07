1260a81bd4ae94eec15e0e1afcfbc099dd308feb
Sudip Roy <59234028+imsudiproy@users.noreply.github.com>
2020-01-08 02:35:25 +0530

    Create 3 sum of two numbers.cpp

---
+++ 3 sum of two numbers.cpp
@@ -0,0 +1,27 @@
+/*
+  Toipc- Sum of two numbers
+  Program by- Sudip Roy
+  Company- SR Group
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    
+    cout << "Enter a number: ";
+    cin >> a;
+    
+    cout <<endl<< "Enter another number: ";
+    cin >> b;
+    
+    sum = a+b;
+    cout <<endl<< "the sum of those numbers is " << sum << endl;
+    
+    return 0;
+}
