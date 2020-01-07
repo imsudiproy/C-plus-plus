bf50b2b40707c338e77cd1adddfca697e58eccee
Sudip Roy <59234028+imsudiproy@users.noreply.github.com>
2020-01-08 02:04:03 +0530

    Create Printing memory address of a variable.cpp

---
+++ Printing memory address of a variable.cpp
@@ -0,0 +1,14 @@
+/*
+   Topic- Printing memory address of a varible
+   Program by- Sudip Roy
+   Company- SR Group
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int var=5;
+   cout<<&var;
+   return 0;
+}
