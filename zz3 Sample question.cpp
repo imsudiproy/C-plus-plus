72422399aca69618f8f91fe84db2d3af0068dfee
Sudip Roy <59234028+imsudiproy@users.noreply.github.com>
2020-01-11 01:07:43 +0530

    Create zz3 Sample question.cpp

---
+++ zz3 Sample question.cpp
@@ -0,0 +1,23 @@
+/*
+   Ques- Print array in reverse
+   Program by- Sudip Roy
+   company- SR Group
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int N,i=0;
+    std::cin>>N;
+    int *A = new int[N];
+    while(std::cin>>A[i++]);
+    while(std::cout<<A[--N]<<' ' && N);
+    delete[] A;
+    return 0;
+}
