0ba53a68c62e638db63a848d2d3cf786e9bc216f
Sudip Roy <59234028+imsudiproy@users.noreply.github.com>
2020-01-12 01:14:02 +0530

    Create 7 Function declaration.cpp

---
+++ 7 Function declaration.cpp
@@ -0,0 +1,21 @@
+/*
+  Topic- Function declaration
+  Program by- Sudip Roy
+  Company- SR Group
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void printSomething()
+{
+  cout << "Hii, It's Sudip here!! " << endl;
+}
+
+int main()
+{
+    printSomething(); // Call the function.
+
+    return 0;
+}
