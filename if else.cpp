98471f2ae17eff3f14c048a926f20719f9d908a3
Sudip Roy <59234028+imsudiproy@users.noreply.github.com>
2020-01-12 01:08:55 +0530

    Create if else.cpp

---
+++ if else.cpp
@@ -0,0 +1,26 @@
+/*
+  Topic- If else statement
+  Program by- Sudip Roy
+  Company- SR Group
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 98;
+    int b = 76;
+    
+    if(a > b)   // Tests if a is greater than b.
+    {  
+        cout << "A is greater" << endl;
+    }
+    else
+    {
+    cout<<"B is greater";
+    }
+   
+    return 0;
+}
